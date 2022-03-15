e47acadf530d482b686a949a3f8e6eb910339634
Harsh Yadav <glorytoharsh@gmail.com>
2022-03-15 10:42:58 +0000

    feat: add abi

---
+++ src/context/index.ts
@@ -0,0 +1,4 @@
+import React , { useEffect, useState, createContext } from 'react';
+import web3 from 'web3';
+
+import { contractAbi, contractAddr } from 'utils/constants';
---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+import contractAbi from './abi.json';
